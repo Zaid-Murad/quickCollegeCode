91405b70d770f0fa27a1377af101b481cf9a15f7
Zaid-Murad <119626142+Zaid-Murad@users.noreply.github.com>
2022-12-01 22:28:45 +0300

    Create FibonacciLessEfficient.cpp

---
+++ FibonacciLessEfficient.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+
+	int n;
+	int n1 = 1;
+	int n2 = 2;
+	int current;
+	int x;
+
+	std::cin >> n;
+	std::cin >> x;
+
+	for (int i = 0; i <= n; i++)
+	{
+
+		if (i < 3)
+			continue;
+
+		current = n1 + n2;
+
+		if (current > x)
+		{
+			std::cout << "not found" << std::endl;
+			return 0;
+		}
+
+		else
+		{
+			if (current == x)
+			{
+				std::cout << "found" << std::endl;
+				return 0;
+			}
+			else
+			{
+				n1 = n2;
+				n2 = current;
+			}
+		}
+	}
+
+}
